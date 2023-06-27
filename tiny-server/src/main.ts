e748164e5937d7fae06bab7751cebf1e7a94aaaf
Traae <bloxtraa@isu.edu>
2023-06-27 16:01:33 -0600

    I set up my enviroment and changed the MongoDB adress -> newer nodeJs needs the IP instead of Localhost, apparently.

--- tiny-server/src/main.ts
+++ tiny-server/src/main.ts
@@ -1,41 +1,49 @@
-import express from 'express';
-import bodyparser from 'body-parser';
-import { MongoClient } from 'mongodb';
-import { AddressInfo } from 'net';
+import express from "express";
+import bodyparser from "body-parser";
+import { MongoClient } from "mongodb";
+import { AddressInfo } from "net";
 
 const app = express();
 
 app.use((_, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE, PATCH');
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, DELETE, PATCH"
+  );
+  next();
 });
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use((req, _res, next) => {
-    console.info(req.method, req.path);
-    next();
+  console.info(req.method, req.path);
+  next();
 });
 
-const mongoDBAddress = process.env.MONGODB_URI || 'mongodb://localhost:27017';
-MongoClient.connect(mongoDBAddress).then(mongoClient => {
+const mongoDBAddress = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017";
+MongoClient.connect(mongoDBAddress)
+  .then((mongoClient) => {
     // ***SET UP DB***
     console.log(`Connected to MongoDB at address: ${mongoDBAddress}`);
-    const db = mongoClient.db('teamups-exercise');
+    const db = mongoClient.db("teamups-exercise");
     const port = process.env.SERVER_PORT || 8080;
 
     // ***START SERVER***
     const server = app.listen(port, () => {
-        const serverAddress = server.address() as AddressInfo;
-        console.log(`Server running on port: ${serverAddress.port}`);
+      const serverAddress = server.address() as AddressInfo;
+      console.log(`Server running on port: ${serverAddress.port}`);
     });
 
     // ***API: PING***
-    app.get('/api/ping', (_, res) => {
-        res.send('pong');
+    app.get("/api/ping", (_, res) => {
+      res.send("pong");
     });
-}).catch(err => {
-    console.error('Unable to connect to MongoDB.', err);
+  })
+  .catch((err) => {
+    console.error("Unable to connect to MongoDB.", err);
     process.exit(1);
-});
+  });
